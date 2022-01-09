bf1868b857f3f877b99c0612bb04ff5ff20b3ee6
Milad Baig-M <87893427+MiladBaig@users.noreply.github.com>
2022-01-09 22:11:50 +0330

    update ackley-plot.py

--- ackley-plot.py
+++ ackley-plot.py
@@ -1,48 +1,3 @@
-# from mpl_toolkits.mplot3d import Axes3D
-# import matplotlib.pyplot as plt
-# from matplotlib import cm
-# import numpy as np
-
-# from ackley import ackley
-
-
-# def plot_ackley_3d():
-#     fig = plt.figure()
-#     ax = fig.gca(projection='3d')
-
-#     # Make data.
-#     X = np.arange(-32, 32, 0.25)
-#     Y = np.arange(-32, 32, 0.25)
-#     X, Y = np.meshgrid(X, Y)
-
-#     a = 20
-#     b = 0.2
-#     c = 2 * np.pi
-
-#     sum_sq_term = -a * np.exp(-b * np.sqrt(X*X + Y*Y) / 2)
-#     cos_term = -np.exp((np.cos(c*X) + np.cos(c*Y)) / 2)
-#     Z = a + np.exp(1) + sum_sq_term + cos_term
-
-#     # Plot the surface.
-#     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-#                            linewidth=0, antialiased=False)
-
-#     # Add a color bar which maps values to colors.
-#     fig.colorbar(surf, shrink=0.5, aspect=5)
-
-#     plt.show()
-
-
-# def plot_ackley_2d():
-#     x = np.arange(-32, 33)
-#     y = [ackley(np.array([x_i])) for x_i in x]
-#     plt.plot(x, y)
-#     plt.show()
-
-
-# if __name__ == "__main__":
-#     plot_ackley_2d()
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -78,18 +33,13 @@ fig = plt.figure()
 ax = Axes3D(fig)
 ax.plot_wireframe(x1, x2, z)
 
-# It's heavy but it has color
-#ax = fig.gca(projection='3d')
-#surf = ax.plot_surface(x1, x2, z, rstride=1, cstride=1, cmap=plt.cm.coolwarm,linewidth=0, antialiased=False)
-#fig.colorbar(surf, shrink=0.5, aspect=7, cmap=plt.cm.coolwarm)
 
 ax.zaxis.set_major_locator(plt.LinearLocator(10))
 ax.zaxis.set_major_formatter(plt.FormatStrFormatter('%.01f'))
 
 plt.show()
 
-# Differencial Evolution
-# http://wildpie.hatenablog.com/entry/20151003/1443863102
+
 
 # initialize
 N = 10
@@ -104,8 +54,7 @@ x2_old = np.copy(x2_init)
 
 index = np.arange(N)
 
-# The reason why taking differencial is that if each agent got become close to optimal,
-# letting the width of update be smaller.
+
 
 
 def animate(x1_new, x2_new, ims, ax2):
